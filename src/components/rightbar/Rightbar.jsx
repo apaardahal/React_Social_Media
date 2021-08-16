775c560d68733af8bc4527b5e19e4ea4fcfb6542
apaardahal <cullaapar@gmail.com>
2021-08-16 23:45:02 +0545

    Rightbar component completed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,86 @@ import './rightbar.css'
 export default function Rightbar() {
     return (
         <div className = "rightbar">
-            rightbar
+            <div className = "rightbarWrapper">
+                <div className = "birthdayContainer">
+                    <img className = "birthdayImg" src = "assets/birthday.png"/>
+                    <span className = "birthdayText"><b>John</b> and <b> 3 others </b> have a birthday today</span>
+                </div>
+                <img className = "rightbarAd" src = "assets/ads.jpg"/>
+                <h4 className = "rightbarTitle">Online Friends</h4>
+                <ul className = "rightbarFriendList">
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                    <li className = "rightbarFriend"> 
+                        <div className = "rightbarProfileImgContainer">
+                             <img  className = "rightbarProfileImg" src = "assets/person/1.jpg" alt = ""/>
+                             <span className = "rightbarOnline"></span>
+                        </div>
+                        <span className = "rightbarUsername">Joyner Lucas</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
